e87aa861f8d7654cdab091247f2067e475ba76ec
markjoeneilsalas <markjoeneil.salas@gmail.com>
2023-07-20 07:28:52 +1000

    modified:   App.js 	new file:   assets/fblogo.png

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Image, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+  };
+
+  const buttonTitle = 'Log In';
+  const transformedTitle = capitalizeFirstLetter(buttonTitle);
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Image source={require('./assets/fblogo.png')} 
+      style={{
+        width: 180, 
+        height: 40,
+        marginTop: -110,
+        marginBottom: 100,
+      }}
+      />
+      <View style={{ marginBottom: 0 }}>
+        <TextInput
+          style={{
+            backgroundColor: 'white',
+            paddingBottom: 10,
+            marginBottom: 10,
+            width: 300
+          }}
+          placeholder="Username"
+        />
+        <TextInput
+          style={{
+            backgroundColor: 'white',
+            paddingBottom: 10,
+            marginBottom: 10,
+            width: 300
+          }}
+          placeholder="Password"
+          secureTextEntry={true}
+        />
+        <View>
+        <Button
+          title={transformedTitle}
+          color="#223B73"
+        />
+        </View>
+        <Text style={{
+          textAlign:'center',
+          color: 'white',
+          textDecorationLine: 'underline',
+        }}>Forgot Password</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#355291',
   },
 });
